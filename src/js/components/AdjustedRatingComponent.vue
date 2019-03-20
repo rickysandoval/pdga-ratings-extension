9406ffc399d1775e100244a4ec2fcc612314471f
Ricky Sandoval <rsandoval@arrow.com>
2019-03-20 15:03:15 -0600

    adjusted rating component

--- src/js/components/AdjustedRatingComponent.vue
+++ src/js/components/AdjustedRatingComponent.vue
@@ -28,28 +28,34 @@ export default {
 };
 </script>
 <template>
-  <div v-if="rounds && rounds.length && adjustedRating">
+  <div>
     <div class="panel-pane panel-adjusted-rating">
       <h2 class="panel-title">Adjusted Rating</h2>
       <div class="pane-content">
-        <div class="adjusted-rating">
-          {{adjustedRating}}
-          <a
-            class="rating-difference"
-            v-on:click.prevent
-            href="#"
-            v-bind:class="{'loss': this.adjustedRating < this.currentRating}"
-          >{{this.adjustedRating < this.currentRating ? '-' : '+'}}{{Math.abs(this.adjustedRating - this.currentRating)}}</a>
-        </div>
+        <template v-if="rounds && rounds.length && adjustedRating">
+          <div class="adjusted-rating">
+            {{adjustedRating}}
+            <a
+              class="rating-difference"
+              v-on:click.prevent
+              href="#"
+              v-bind:class="{'loss': this.adjustedRating < this.currentRating}"
+            >{{this.adjustedRating < this.currentRating ? '-' : '+'}}{{Math.abs(this.adjustedRating - this.currentRating)}}</a>
+          </div>
+
+          <p>Based on {{rounds.length}} new rounds:</p>
+          <ul class="info-list">
+            <li v-for="round in rounds">
+              <span v-if="round.tournamentName">{{round.tournamentName}}</span>
+              <span v-if="round.roundNumber">Rd {{round.roundNumber}} -</span>
+              <strong>{{round.roundRating}}</strong>
+            </li>
+          </ul>
+        </template>
+        <template v-if="!(rounds && rounds.length && adjustedRating)">
+            <p>Try adding some rounds to see how it will affect your rating</p>
+        </template>
       </div>
-      <p>Based on {{rounds.length}} new rounds:</p>
-      <ul class="info-list">
-        <li v-for="round in rounds">
-          <span v-if="round.tournamentName">{{round.tournamentName}} </span>
-          <span v-if="round.roundNumber">Rd {{round.roundNumber}} - </span>
-          <strong>{{round.roundRating}}</strong>
-        </li>
-      </ul>
     </div>
     <div class="pane-separator"></div>
   </div>
