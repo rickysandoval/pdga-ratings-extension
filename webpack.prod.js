dee66ffdf86363303b842fcb50e32cfea1dc6b06
Ricky Sandoval <rsandoval@arrow.com>
2019-04-03 12:03:28 -0600

    icons

--- webpack.prod.js
+++ webpack.prod.js
@@ -6,19 +6,19 @@ const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
 
 module.exports = merge(common, {
-    mode: 'development',
+    mode: 'production',
     devtool: 'source-map',
     output: {
-        path: path.resolve(__dirname, `dist-dev/js`),
+        path: path.resolve(__dirname, `dist-prod/js`),
         filename: "[name].js"
     },
     plugins: [
         new CopyWebpackPlugin([{
             from: './src/manifest.json',
-            to: path.resolve(__dirname, `dist-dev`)
+            to: path.resolve(__dirname, `dist-prod`)
         }, {
             from: './src/icons',
-            to: path.resolve(__dirname, `dist-dev/icons`)
+            to: path.resolve(__dirname, `dist-prod/icons`)
         }, ]),
         new VueLoaderPlugin()
     ]
